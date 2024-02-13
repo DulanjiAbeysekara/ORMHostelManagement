e3224feb6ed0c82b378c25843cb61b6e85dc87c4
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2024-02-14 02:21:05 +0530

    desinging home page

--- src/main/java/lk/ijse/hostelMangement/controller/HomePageController.java
+++ src/main/java/lk/ijse/hostelMangement/controller/HomePageController.java
@@ -1,4 +1,45 @@
 package lk.ijse.hostelMangement.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
 public class HomePageController {
+
+    @FXML
+    private AnchorPane homepagePane;
+
+    @FXML
+    private Button resrvationBtn;
+
+    @FXML
+    private Button roomBtn;
+
+    @FXML
+    private Button studentBtn;
+
+    @FXML
+    private Button userBtn;
+
+    @FXML
+    void resrvationBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void roomBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void studentBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void userBtnOnAction(ActionEvent event) {
+
+    }
+
 }
